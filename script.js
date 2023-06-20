60f5052538b18b73c4132c356c21147aa858e650
prithvijedipatil <prithvipatil09@gmail.com>
2023-06-20 14:48:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
 
+
+    const fname = document.querySelector('input[name="fname"]');
+    const lname = document.querySelector('input[name="flname"]');
+
+    const firstname = fname.value;
+    const lasttname = lname.value;
+    alert(`First name: ${firstname}\n Last name: ${lasttname}`);
+
 }
